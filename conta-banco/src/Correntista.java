36cd8257e05ebea56c0e7a9e8874d567681cb599
César Fraga <cesarfraaga@gmail.com>
2024-05-13 12:22:29 -0300

    delete first files

---
+++ conta-banco/src/Correntista.java
@@ -0,0 +1,10 @@
+public class Correntista {
+
+    String nomeCorrentista, agencia;
+    int numeroConta;
+    double saldo;
+
+    public void CorrentistaOuNao() {
+        System.out.println("Já é correntista? Y = SIM | N = Não");
+    }
+}
